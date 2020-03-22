21d786047c27eb990652ac100b6f75865e113cb3
VincentFokker <44567443+VincentFokker@users.noreply.github.com>
2020-03-22 20:53:07 +0100

    Update stonks.py

--- Impact on Economics/stonks.py
+++ Impact on Economics/stonks.py
@@ -1,56 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 from datetime import datetime
 import plotly.graph_objects as go
+import streamlit as st
 
 
 # # Load the data
 # Historical data for the dutch stock market is loaded.
-
-# In[2]:
-
-
 AEX = pd.read_csv('AEX.csv')
 date_format = '%Y-%m-%d'
 AEX.Date = [datetime.strptime(date, date_format) for date in AEX.Date]
 AEX['day_diff'] = AEX.Close - AEX.Open
 AEX['winloss'] = abs(AEX.day_diff) / AEX.Open
-AEX.head()
 
-
-# In[3]:
-
-
+#hang seng
 HSI = pd.read_csv('HSI.csv')
 date_format = '%Y-%m-%d'
 HSI.Date = [datetime.strptime(date, date_format) for date in HSI.Date]
 HSI['day_diff'] = HSI.Close - HSI.Open
 HSI['winloss'] = abs(HSI.day_diff) / HSI.Open
-HSI.head()
 
-
-# In[4]:
-
-
+#Shanghai Composite
 SHCI = pd.read_csv('SHCI.csv')
 date_format = '%Y-%m-%d'
 SHCI.Date = [datetime.strptime(date, date_format) for date in SHCI.Date]
 SHCI['day_diff'] = SHCI.Close - SHCI.Open
 SHCI['winloss'] = abs(SHCI.day_diff) / SHCI.Open
-SHCI.head()
 
 
-# # Functions used
-
-# In[5]:
-
-
+# # Functions
 #lets make it more interactive with plotly
 def plot_period_stocks(AEX, start_date, end_date, option_list, title):
     """plots the stocks for a given time period"""
@@ -121,10 +99,7 @@ def plot_shape_in_fig(fig, start_date, end_date, color='green', low=350, high=57
 
 
 # # General Overview
-
-# In[6]:
-
-
+st.title('General Overview')
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'Overview of AEX for past 28 years'
 fig, AEX_s = plot_period_stocks(AEX, AEX.Date.min().__format__(date_format), AEX.Date.max().__format__(date_format), option_list, title)
@@ -140,7 +115,6 @@ fig = plot_line_in_fig(fig, AEX_s, '2020-02-18', 'Covid-19', 40)
 fig
 
 
-# In[7]:
 
 
 option_list = ['Close', 'Open', 'High', 'Low']
@@ -158,8 +132,6 @@ fig = plot_line_in_fig(fig, HSI_s, '2020-02-18', 'Covid-19', 2000)
 fig
 
 
-# In[8]:
-
 
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'Overview of Shanghai Composite (China) for past 30 years'
@@ -175,19 +147,16 @@ fig = plot_line_in_fig(fig, SHCI_s, '2018-09-20', 'Stock Market Downturn')
 fig = plot_line_in_fig(fig, SHCI_s, '2020-02-18', 'Covid-19', 400)
 fig
 
+st.write('Looking at these effects, the Dotcom in 2001 and the 2008 market crash had the higest impact on the AEX next to the Covid-19. Because of this we will make an analysis of these crashes (financially forced) and compare them to the Covid-19 situation.')
 
-# Looking at these effects, the Dotcom in 2001 and the 2008 market crash had the higest impact on the AEX next to the Covid-19. Because of this we will make an analysis of these crashes (financially forced) and compare them to the Covid-19 situation.
+st.title('Analysis of stock crash')
+st.write('We compare the graph for the crash with 2001 and 2008 for the dot.com bubble and the 2008 market crash')
 
-# # Analysis of stock crash
-# We compare the graph for the crash with 2001 and 2008 for the dot.com bubble and the 2008 market crash
+st.markdown('## For the AEX')
+st.markdown('Check AEX Stocks from 01-01-2020 till today.')
 
-# ## For the AEX
+st.markdown('### For Covid-19')
 
-# In[9]:
-
-
-##### For Covid-19
-#### check AEX Stocks from 01-01-2020 till today.
 
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
@@ -202,11 +171,7 @@ fig = plot_shape_in_fig(fig, '2020-03-12', '2020-03-16', high=650)
 
 fig
 
-
-# In[10]:
-
-
-## For comparisation, stock crash of 2008
+st.markdown('### For comparisation, stock crash of 2008')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'The stock market crash in 2008'
@@ -216,14 +181,9 @@ fig = plot_shape_in_fig(fig, '2007-12-12', '2007-12-21')
 fig = plot_shape_in_fig(fig, '2007-12-22', '2008-01-03')
 fig = plot_shape_in_fig(fig, '2008-01-10', '2008-01-15')
 fig = plot_shape_in_fig(fig, '2008-01-21', '2008-01-25')
-
 fig
 
-
-# In[11]:
-
-
-## For comparisation, stock crash of 2008
+st.markdown('### For comparisation, the dotcom bubble of 2001')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'The Dotcom bubble 2001 - highest drop in 2001'
@@ -233,26 +193,20 @@ fig = plot_shape_in_fig(fig, '2001-08-16', '2001-08-23')
 fig = plot_shape_in_fig(fig, '2001-08-24', '2001-08-31')
 fig = plot_shape_in_fig(fig, '2001-09-09', '2001-09-14')
 fig = plot_shape_in_fig(fig, '2001-09-20', '2001-09-24')
-
-
 fig
 
 
-# ### Re-occurring themes
-# All the graphs are of a same time period and show similar trends:
-# - There is a period of slight drop in stocks at the beginning
-# - Then a small rise in overal stocks
-# - then stocks start to drop, but there is an seemfull recovery in the middle
-# - then the drop steeply drops to 400 or less.
+st.markdown('## Re-occurring themes')
+st.markdown('All the graphs are of a same time period and show similar trends:')
+st.markdown('- There is a period of slight drop in stocks at the beginning')
+st.markdown('- Then a small rise in overal stocks')
+st.markdown('- then stocks start to drop, but there is an seemfull recovery in the middle')
+st.markdown('- then the drop steeply drops to 400 or less.')
 
-# ## For the HSI
+st.markdown('## For the HSI')
+st.markdown('Check HSI Stocks from 01-01-2020 till today.')
 
-# In[12]:
-
-
-##### For Covid-19
-#### check HSI Stocks from 01-01-2020 till today.
-
+st.markdown('### For Covid-19')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'COVID-19'
@@ -264,11 +218,8 @@ fig = plot_shape_in_fig(fig, '2020-03-12', '2020-03-16', high=30000, low=20000)
 
 fig
 
-
-# In[13]:
-
-
-## For comparisation, stock crash of 2008
+st.markdown('### For comparisation, stock crash of 2008')
+ 
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'The stock market crash in 2008'
@@ -281,11 +232,7 @@ fig = plot_shape_in_fig(fig, '2008-01-21', '2008-01-25', high=30000, low=20000)
 
 fig
 
-
-# In[14]:
-
-
-## For comparisation, stock crash of 2008
+st.markdown('### For comparisation, stock crash of 2008')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'The Dotcom bubble 2001 - highest drop in 2001'
@@ -295,19 +242,12 @@ fig = plot_shape_in_fig(fig, '2001-08-16', '2001-08-23', high=13000, low=8000)
 fig = plot_shape_in_fig(fig, '2001-08-24', '2001-08-31', high=13000, low=8000)
 fig = plot_shape_in_fig(fig, '2001-09-09', '2001-09-14', high=13000, low=8000)
 fig = plot_shape_in_fig(fig, '2001-09-20', '2001-09-24', high=13000, low=8000)
-
-
 fig
 
-
-# ## for Shanghai Composite (china)
-
-# In[15]:
-
-
-##### For Covid-19
-#### check HSI Stocks from 01-01-2020 till today.
-
+st.markdown('## For Shanghai Composite (china)')
+st.markdown('check HSI Stocks from 01-01-2020 till today.')
+st.markdown('### For Covid-19')
+#### 
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'COVID-19'
@@ -319,11 +259,7 @@ fig = plot_shape_in_fig(fig, '2020-03-05', '2020-03-16', high=3200, low=2600)
 
 fig
 
-
-# In[16]:
-
-
-## For comparisation, stock crash of 2008
+st.markdown('### For comparisation, stock crash of 2008')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'The stock market crash in 2008'
@@ -336,11 +272,7 @@ fig = plot_shape_in_fig(fig, '2008-01-22', '2008-01-25', high=5600, low=4000)
 
 fig
 
-
-# In[17]:
-
-
-## For comparisation, stock crash of 2008
+st.markdown('### For comparisation, The dotcom bubble in 2001')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'The Dotcom bubble 2001 - highest drop in 2001'
@@ -350,14 +282,11 @@ fig = plot_shape_in_fig(fig, '2001-08-16', '2001-08-23', high=2000, low=1500)
 fig = plot_shape_in_fig(fig, '2001-08-24', '2001-08-31', high=2000, low=1500)
 fig = plot_shape_in_fig(fig, '2001-09-09', '2001-09-14', high=2000, low=1500)
 fig = plot_shape_in_fig(fig, '2001-09-20', '2001-09-24', high=2000, low=1500)
-
-
 fig
 
 
-# # Comparing to other outbreaks
-
-# In[18]:
+st.title('Comparing to other outbreaks')
+st.write('First we will have a look at the full market for the AEX, Hang Seng and China Composite for the different epedemic outbreaks.')
 
 
 option_list = ['Close', 'Open', 'High', 'Low']
@@ -379,9 +308,6 @@ fig = plot_line_in_fig(fig, AEX_s, '2020-02-18', 'Covid-19')
 fig
 
 
-# In[19]:
-
-
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'Overview of Hang Seng (HSI) for past 28 years'
 fig, HSI_s = plot_period_stocks(HSI, HSI.Date.min().__format__(date_format), HSI.Date.max().__format__(date_format), option_list, title)
@@ -399,9 +325,6 @@ fig = plot_line_in_fig(fig, HSI_s, '2019-06-03', 'Measles', 1000)
 fig = plot_line_in_fig(fig, HSI_s, '2020-02-18', 'Covid-19')
 
 fig
-
-
-# In[20]:
 
 
 option_list = ['Close', 'Open', 'High', 'Low']
@@ -423,16 +346,13 @@ fig = plot_line_in_fig(fig, SHCI_s, '2020-02-18', 'Covid-19')
 fig
 
 
-# ## Conclusions
-# Some Effects rights after certain viral outbreaks can be seen. Look for example at SARS, Avian Flu, H1N1, MERS, Zika. Ofcourse we can not make sound conclusions from this, but the observed effect does seem interesting. To get a better understanding we will zoom in a bit more on SARS and MERS, which were viral outbreaks with large amounts of deaths as well, with SARS beeing maybe the most similar to Covid-19.
+st.markdown('## Conclusions')
+st.markdown('Some Effects rights after certain viral outbreaks can be seen. Look for example at SARS, Avian Flu, H1N1, MERS, Zika. Ofcourse we can not make sound conclusions from this, but the observed effect does seem interesting. To get a better understanding we will zoom in a bit more on SARS and MERS, which were viral outbreaks with large amounts of deaths as well, with SARS beeing maybe the most similar to Covid-19.')
 
-# ### SARS effect
-# We will look at the three markets, right after the SARS outbreak.
+st.title('SARS effect')
+st.write('We will look at the three markets, right after the SARS outbreak.')
 
-# In[21]:
-
-
-## For comparisation during the SARS outbreak
+st.markdown('## For comparisation during the SARS outbreak')
 ## options are: 'Close', 'Open', 'High', 'Low', 'day_diff', 'winloss'
 option_list = ['Close', 'Open', 'High', 'Low']
 title = 'AEX during the 2002 /2003 /2004 SARS outbreak (also dotcom bubble)'
@@ -441,9 +361,6 @@ fig, AEX_s = plot_period_stocks(AEX, '2002-11-01', '2003-05-01', option_list, ti
 # add label and vertical for the sars outbreak date
 fig = plot_line_in_fig(fig, AEX_s, '2002-11-18', 'SARS')
 fig
-
-
-# In[22]:
 
 
 ## For comparisation during the SARS outbreak
@@ -457,7 +374,6 @@ fig = plot_line_in_fig(fig, HSI_s, '2002-11-18', 'SARS')
 fig
 
 
-# In[23]:
 
 
 ## For comparisation during the SARS outbreak
@@ -471,18 +387,12 @@ fig = plot_line_in_fig(fig, SHCI_s, '2002-11-18', 'SARS')
 fig
 
 
-# # more stuff to think about:
-
-# In[24]:
-
+st.title('more stuff to think about:') 
 
 option_list = ['day_diff']
 title=' Daily difference in value'
 fig, AEX_s = plot_period_stocks(AEX, '2020-01-02', datetime.today().__format__(date_format), option_list, title)
 fig
-
-
-# In[25]:
 
 
 option_list = ['winloss']
